33b7313d84abf17ac28dd5cb769abb62d7b1ff1a
agosfrixione <agosfrixione@gmail.com>
2023-01-03 20:43:04 -0300

    configurando para deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
+  DB_USER, DB_PASSWORD, DB_DEPLOY
 } = process.env;
 
 // var sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
@@ -11,7 +11,7 @@ const {
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 
-var sequelize = new Sequelize(DB_DEPLOY, {
+var sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_DEPLOY}/videogames_ml66`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
